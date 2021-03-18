a7a969b031f6d43c5f14a208818c43828ef5fedf
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-03-18 13:09:19 -0700

    PSI Search

--- api/src/main/java/ca/bc/gov/educ/api/psi/controller/PsiController.java
+++ api/src/main/java/ca/bc/gov/educ/api/psi/controller/PsiController.java
@@ -2,12 +2,16 @@ package ca.bc.gov.educ.api.psi.controller;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -57,4 +61,22 @@ public class PsiController {
     	logger.debug("getPSIDetails : ");
         return response.GET(psiService.getPSIDetails(psiCode));
     }
+    
+    @GetMapping(EducPsiApiConstants.GET_PSI_SEARCH_MAPPING)
+    @PreAuthorize(PermissionsContants.READ_PSI_INFO)
+    public ResponseEntity<List<Psi>> getPSIByParams(
+    		@RequestParam(value = "psiName", required = false) String psiName,
+    		@RequestParam(value = "pageNo", required = false,defaultValue = "0") Integer pageNo, 
+            @RequestParam(value = "pageSize", required = false,defaultValue = "20") Integer pageSize) {
+		OAuth2AuthenticationDetails auth = (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails(); 
+    	String accessToken = auth.getTokenValue();
+    	if((StringUtils.isNotBlank(psiName) && psiName.length() < 3)) {
+    		validation.addError("Error in PSI Search");
+    	}
+    	if(validation.hasErrors()) {
+    		validation.stopOnErrors();
+    		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    	}
+		return response.GET(psiService.getPSIByParams(psiName,pageNo,pageSize,accessToken));
+	}
 }
--- api/src/main/java/ca/bc/gov/educ/api/psi/repository/PsiRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/psi/repository/PsiRepository.java
@@ -3,6 +3,7 @@ package ca.bc.gov.educ.api.psi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ca.bc.gov.educ.api.psi.model.entity.PsiEntity;
@@ -11,5 +12,9 @@ import ca.bc.gov.educ.api.psi.model.entity.PsiEntity;
 public interface PsiRepository extends JpaRepository<PsiEntity, String> {
 
     List<PsiEntity> findAll();
+
+    @Query(value="SELECT si.* FROM TAB_POSTSEC si where "
+			+ "(:psiName is null or si.PSI_NAME like %:psiName%) and ROWNUM <= 50",nativeQuery = true)	
+	List<PsiEntity> searchForPSI(String psiName);
 
 }
